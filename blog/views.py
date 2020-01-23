9f04afc7635e21e1815086f30db363e7e5061794
Mohammed Shoaib <shoaibbilal101@gmail.com>
2020-01-23 16:37:15 +0500

    Video 81

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def blogHome(request):
+    return render(request, 'blog/blogHome.html')
+
+    # return HttpResponse("This is blogHome. we will keep all the blog post here")
+def blogPost(request, slug):
+     return render(request, 'blog/blogPost.html')
+    # return HttpResponse(f"This is BlogPost: {slug}")
