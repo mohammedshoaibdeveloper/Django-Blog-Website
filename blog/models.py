bf066ae0790771452c66da6227fbddc4d2f78bb8
Mohammed Shoaib <shoaibbilal101@gmail.com>
2020-01-24 07:15:58 +0500

    Video 82

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+# Create your models here.
+class  Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author_name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=130)
+    timeStamp = models.DateTimeField(blank=True)
+
+    def __str__(self):
+        return self.title +  ' written by ' + self.author_name 
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, HttpResponse
+from blog.models import Post
 
 # Create your views here.
 def blogHome(request):
-    return render(request, 'blog/blogHome.html')
+    allPosts = Post.objects.all()
+    context={'allPosts': allPosts}
+    return render(request, 'blog/blogHome.html',context)
+    
+   
 
-    # return HttpResponse("This is blogHome. we will keep all the blog post here")
+    
 def blogPost(request, slug):
      return render(request, 'blog/blogPost.html')
-    # return HttpResponse(f"This is BlogPost: {slug}")
+    
